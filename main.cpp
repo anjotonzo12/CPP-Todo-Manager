f43143d7046ca4a26d4b222e4b99ae615e9bbba2
Pupler <misha.reak45@gmail.com>
2025-12-02 19:29:03 +0100

    Feat: implement interactive command menu

--- main.cpp
+++ main.cpp
@@ -13,12 +13,30 @@ struct Task {
 };
 
 int main() {
+    string input;
     vector<Task> tasks;
     tasks.push_back(Task("Get a job", false));
     tasks.push_back(Task("Learn C#", false));
     cout << "\n====C++ TODO MANAGER====" << endl;
-    for (int i = 0; i < tasks.size(); i++) {
-        cout << tasks[i].description << " -- " << (tasks[i].is_completed ? "Yes" : "No") << endl;
+    cout << "Commands: list, exit" << endl;
+
+    while (true) {
+        cout << "> ";
+        getline(cin, input);
+
+        if (input.empty()) {
+            continue;
+        } else if (input == "list") {
+            for (int i = 0; i < tasks.size(); i++) {
+                cout << tasks[i].description << " -- " << (tasks[i].is_completed ? "Yes" : "No") << endl;
+            }
+        } else if (input == "exit") {
+            break;
+        } else {
+            cout << "Invalid command!";
+        }
+
+        cout << endl;
     }
 
     return 0;

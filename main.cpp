5f401091d1c5dfc73c834856510292b3e7621373
Pupler <misha.reak45@gmail.com>
2025-12-02 19:04:08 +0100

    Feat: implement basic task management system

--- main.cpp
+++ main.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 struct Task {
     string description;
     bool is_completed;
 
-    Task(const string& desc) {
+    Task(const string& desc, bool is_compltd = false) {
         description = desc;
-        is_completed = false;
+        is_completed = is_compltd;
     }
 };
 
 int main() {
-    cout << "\n====C++ Todo Manager====" << endl;
+    vector<Task> tasks;
+    tasks.push_back(Task("Get a job", false));
+    tasks.push_back(Task("Learn C#", false));
+    cout << "\n====C++ TODO MANAGER====" << endl;
+    for (int i = 0; i < tasks.size(); i++) {
+        cout << tasks[i].description << " -- " << (tasks[i].is_completed ? "Yes" : "No") << endl;
+    }
 
     return 0;
 }
